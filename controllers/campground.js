3cb08bf6ae9ef5c357ef47a9adb7a1ad5384d070
nv.hai <105141081+hai0111@users.noreply.github.com>
2023-09-18 23:05:57 +0700

    campground controller

---
+++ controllers/campground.js
@@ -0,0 +1,86 @@
+const Campground = require('../models/campground')
+const ExpressError = require('../utils/ExpressError')
+const CampGround = require('../models/campground')
+const seedDB = require('../seeds')
+
+module.exports.index = async (req, res) => {
+	const camps = await Campground.find({})
+	res.render('campgrounds', {
+		camps,
+		title: 'All campgrounds',
+		nav: true,
+	})
+}
+
+module.exports.renderNewForm = async (req, res) => {
+	res.render('campgrounds/new', {
+		nav: true,
+	})
+}
+
+module.exports.detail = async (req, res) => {
+	const { id } = req.params
+	if (!id) return
+	const camp = await CampGround.findById(id).populate({
+		path: 'reviews',
+		populate: {
+			path: 'author',
+		},
+	})
+
+	camp?.reviews.forEach((rv) => {
+		rv.isAuthor = req.user?.id && rv.author?.equals(req.user?.id)
+	})
+
+	if (!camp) throw new ExpressError(404, 'Campground not found')
+
+	res.locals.isAuthor = req.user && camp.author?.id === req.user?.id
+	res.render('campgrounds/detail', { camp, title: camp?.title, nav: true })
+}
+module.exports.renderUpdateForm = async (req, res) => {
+	const { id } = req.params
+	const camp = await CampGround.findById(id)
+	res.render('campgrounds/update', {
+		camp,
+		title: `Update ${camp?.title}`,
+		nav: true,
+	})
+}
+module.exports.update = async (req, res) => {
+	const { id } = req.params
+
+	await CampGround.findByIdAndUpdate(id, req.body).then((camp) => camp.save())
+
+	req.flash('message', {
+		type: 'success',
+		text: 'Update completed successfully',
+	})
+	res.redirect(`/campgrounds/${id}`)
+}
+module.exports.delete = async (req, res) => {
+	const { id } = req.params
+
+	await CampGround.findByIdAndDelete(id)
+
+	req.flash('message', {
+		type: 'success',
+		text: 'Delete completed successfully',
+	})
+	res.redirect('/campgrounds')
+}
+module.exports.create = async (req, res) => {
+	const { body } = req
+	const c = new CampGround({ ...body, author: req.user._id })
+	await c.save()
+	req.flash('message', {
+		type: 'success',
+		text: 'Create completed successfully',
+	})
+
+	res.redirect(`/campgrounds/${c._id}`)
+}
+
+module.exports.reset = async (req, res) => {
+	await seedDB()
+	res.redirect('/campgrounds')
+}
--- routers/campgrounds.js
+++ routers/campgrounds.js
@@ -2,11 +2,9 @@ const express = require('express')
 
 const catchAsync = require('../utils/catchAsync')
 
-// Utils
-const ExpressError = require('../utils/ExpressError')
+const campground = require('../controllers/campground')
 
-const CampGround = require('../models/campground')
-const seedDB = require('../seeds')
+// Utils
 const reviewRouter = require('./reviews')
 const {
 	authenticate,
@@ -18,64 +16,17 @@ const campgroundRouter = express.Router()
 
 campgroundRouter.use('/:id/reviews', reviewRouter)
 
-campgroundRouter.get(
-	'/',
-	catchAsync(async (req, res) => {
-		const camps = await CampGround.find({})
-		res.render('campgrounds', {
-			camps,
-			title: 'All campgrounds',
-			nav: true,
-		})
-	})
-)
+campgroundRouter.get('/', catchAsync(campground.index))
 
-campgroundRouter.get(
-	'/new',
-	authenticate,
-	catchAsync(async (req, res) => {
-		res.render('campgrounds/new', {
-			nav: true,
-		})
-	})
-)
+campgroundRouter.get('/new', authenticate, catchAsync(campground.renderNewForm))
 
-campgroundRouter.get(
-	'/:id',
-	catchAsync(async (req, res) => {
-		const { id } = req.params
-		if (!id) return
-		const camp = await CampGround.findById(id).populate({
-			path: 'reviews',
-			populate: {
-				path: 'author',
-			},
-		})
-
-		camp?.reviews.forEach((rv) => {
-			rv.isAuthor = req.user?.id && rv.author?.equals(req.user?.id)
-		})
-
-		if (!camp) throw new ExpressError(404, 'Campground not found')
-
-		res.locals.isAuthor = req.user && camp.author?.id === req.user?.id
-		res.render('campgrounds/detail', { camp, title: camp?.title, nav: true })
-	})
-)
+campgroundRouter.get('/:id', catchAsync(campground.detail))
 
 campgroundRouter.get(
 	'/:id/update',
 	authenticate,
 	isAuthorCampground,
-	catchAsync(async (req, res) => {
-		const { id } = req.params
-		const camp = await CampGround.findById(id)
-		res.render('campgrounds/update', {
-			camp,
-			title: `Update ${camp?.title}`,
-			nav: true,
-		})
-	})
+	catchAsync(campground.renderUpdateForm)
 )
 
 campgroundRouter.put(
@@ -83,59 +34,23 @@ campgroundRouter.put(
 	authenticate,
 	isAuthorCampground,
 	validateCampground,
-	catchAsync(async (req, res) => {
-		const { id } = req.params
-
-		await CampGround.findByIdAndUpdate(id, req.body).then((camp) => camp.save())
-
-		req.flash('message', {
-			type: 'success',
-			text: 'Update completed successfully',
-		})
-		res.redirect(`/campgrounds/${id}`)
-	})
+	catchAsync(campground.update)
 )
 
 campgroundRouter.delete(
 	'/:id',
 	authenticate,
 	isAuthorCampground,
-	catchAsync(async (req, res) => {
-		const { id } = req.params
-
-		await CampGround.findByIdAndDelete(id)
-
-		req.flash('message', {
-			type: 'success',
-			text: 'Delete completed successfully',
-		})
-		res.redirect('/campgrounds')
-	})
+	catchAsync(campground.delete)
 )
 
 campgroundRouter.post(
 	'/',
 	authenticate,
 	validateCampground,
-	catchAsync(async (req, res) => {
-		const { body } = req
-		const c = new CampGround({ ...body, author: req.user._id })
-		await c.save()
-		req.flash('message', {
-			type: 'success',
-			text: 'Create completed successfully',
-		})
-
-		res.redirect(`/campgrounds/${c._id}`)
-	})
+	catchAsync(campground.create)
 )
 
-campgroundRouter.post(
-	'/reset',
-	catchAsync(async (req, res) => {
-		await seedDB()
-		res.redirect('/campgrounds')
-	})
-)
+campgroundRouter.post('/reset', catchAsync(campground.reset))
 
 module.exports = campgroundRouter
