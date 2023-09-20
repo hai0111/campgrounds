b722625cf39683bb71e7a636ff4bf62286967693
nv.hai <105141081+hai0111@users.noreply.github.com>
2023-09-20 16:13:12 +0700

    bổ sung rating star

--- routers/auth.js
+++ routers/auth.js
@@ -5,20 +5,21 @@ const auth = require('../controllers/auth')
 const { validateUser } = require('../middleware')
 const authRouter = express.Router()
 
-authRouter.get('/register', auth.renderRegisterForm)
+authRouter
+	.route('/register')
+	.get(auth.renderRegisterForm)
+	.post(validateUser, catchAsync(auth.register))
 
-authRouter.post('/register', validateUser, catchAsync(auth.register))
-
-authRouter.get('/login', auth.renderLoginForm)
-
-authRouter.post(
-	'/login',
-	passport.authenticate('local', {
-		failureRedirenect: '/login',
-		failureFlash: true,
-	}),
-	auth.loginSuccess
-)
+authRouter
+	.route('/login')
+	.get(auth.renderLoginForm)
+	.post(
+		passport.authenticate('local', {
+			failureRedirenect: '/login',
+			failureFlash: true,
+		}),
+		auth.loginSuccess
+	)
 
 authRouter.get('/logout', auth.logout)
 
