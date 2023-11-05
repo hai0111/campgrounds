e82798614c805634ee5e405c07747607e98f1c0d
nv.hai <105141081+hai0111@users.noreply.github.com>
2023-11-05 12:17:25 +0700

    update seed

--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,4 @@
 // const mongoose = require('mongoose')
-const { default: axios } = require('axios')
 const CampGround = require('../models/campground')
 
 const cities = require('./cities')
@@ -23,33 +22,27 @@ const seedDB = async () => {
 	for (let i = 0; i < 50; i++) {
 		const random1000 = Math.floor(Math.random() * 1000)
 		const randomPrice = Math.floor(Math.random() * 10) + 20
-		const location = `${cities[random1000].city} , ${cities[random1000].state}`
-
-		mapbox
-			.forwardGeocode({
-				query: location,
-				limit: 1,
-			})
-			.send()
-			.then((geocodeResponse) => {
-				const { geometry } = geocodeResponse.body.features[0]
-				const c = new CampGround({
-					author: '6502b6ae9ea4439a17b3efd3',
-					location,
-					geometry,
-					title: `${sample(descriptors)} ${sample(places)}`,
-					description:
-						'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id provident suscipit iusto nostrum. Quis itaque necessitatibus natus, similique error harum provident ab, dolore ad, obcaecati doloremque at beatae? Minus, autem.',
-					images: [
-						{
-							url: 'https://source.unsplash.com/collection/9046579',
-						},
-					],
-					price: randomPrice,
-					reviews: [],
-				})
-				c.save()
-			})
+		const city = cities[random1000]
+		const location = `${city.city} , ${city.state}`
+		const c = new CampGround({
+			author: '6502b6ae9ea4439a17b3efd3',
+			location,
+			geometry: {
+				type: 'Point',
+				coordinates: [city.longitude, city.latitude],
+			},
+			title: `${sample(descriptors)} ${sample(places)}`,
+			description:
+				'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id provident suscipit iusto nostrum. Quis itaque necessitatibus natus, similique error harum provident ab, dolore ad, obcaecati doloremque at beatae? Minus, autem.',
+			images: [
+				{
+					url: 'https://source.unsplash.com/collection/9046579',
+				},
+			],
+			price: randomPrice,
+			reviews: [],
+		})
+		c.save()
 	}
 }
 
